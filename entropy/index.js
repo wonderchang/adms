7fc76d86e11e11667b27fc8d5148343de32120fd
Wonder Chang <iwonder.tw@gmail.com>
2016-01-29 11:27:43 +0800

    Update 2016-01-29T03:27:39.970Z

---
+++ entropy/index.js
@@ -0,0 +1,39 @@
+var lowerBound, upperBound, x, normalDist, entropyDist, entropy, i$, i, p, e;
+lowerBound = -5;
+upperBound = 5;
+x = [];
+normalDist = [];
+entropyDist = [];
+entropy = 0;
+for (i$ = lowerBound; i$ <= upperBound; i$ += 0.1) {
+  i = i$;
+  x.push(i);
+  normalDist.push(p = (1 / Math.sqrt(2 * Math.PI)) * Math.exp(-i * i / 2));
+  if (p === 0) {
+    e = null;
+  } else {
+    e = -p * (Math.log(p) / Math.log(2));
+  }
+  entropyDist.push(e);
+  if (e !== null) {
+    entropy += e;
+  }
+}
+c3.generate({
+  bindto: '#plot-normal',
+  data: {
+    x: 'x',
+    columns: [['x'].concat(x), ['normal distribution'].concat(normalDist)]
+  }
+});
+c3.generate({
+  bindto: '#plot-entropy',
+  data: {
+    x: 'x',
+    columns: [['x'].concat(x), ['-p log p'].concat(entropyDist)]
+  },
+  color: {
+    pattern: ['#ff7f0e']
+  }
+});
+$('#entropy').html("Entropy = " + entropy);
